28466761a97911e3a3275ae4a343e9b166222568
Maryan08 <mar08khymych@gmail.com>
2020-05-05 20:53:22 +0300

    five

---
+++ OOP Lab 2.5/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace OOP_Lab_2._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Кількість рядків: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Кількість стовпців:");
+            int m = int.Parse(Console.ReadLine());
+            Random rnd = new Random();
+            float[,] a = new float[n,m];
+            
+            float[] max = new float[n];
+            float[] max1 = new float[n];
+            for (int i = 0; i < n; i++)
+                for(int j=0;j<m;j++)
+            {
+                    a[i, j] = rnd.Next(-100, 100);
+                    Console.WriteLine("a[{0}][{1}] = {2}", i, j, a[i, j]);
+
+            }
+        
+            for(int k = 0; k < n; k++)
+            {
+                
+                    max[k] = a[k, 0];
+               
+
+            }
+            for (int k = 0; k < n; k++)
+            {
+                for (int i = k; i < k+1; i++)
+                    for (int j = 0; j < m; j++)
+
+                    {
+                        if (a[i, j] > max[k])
+                        {
+                            max[k] = a[i, j];
+                        }
+                    }
+            }
+
+            for (int f = 0; f < n; f++)
+            {
+
+                
+                Console.WriteLine("max[{0}] = {1}", f, max[f]);
+
+            }
+        }
+    }
+}
